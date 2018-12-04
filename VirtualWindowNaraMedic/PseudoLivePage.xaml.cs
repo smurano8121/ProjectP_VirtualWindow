2549204ecd04c385a32399198e5d0f392ac35912
Shota_Murano <smurano@mikilab.doshisha.ac.jp>
2018-12-04 15:50:50 +0900

    擬似窓画面切り替え滑らかにした

--- VirtualWindowNaraMedic/PseudoLivePage.xaml.cs
+++ VirtualWindowNaraMedic/PseudoLivePage.xaml.cs
@@ -28,37 +28,39 @@ namespace VirtualWindowUWP
         // To get video library, we have to declare the function in app manifest.
         private static StorageFolder videoLibrary = KnownFolders.VideosLibrary;
         // Media element static object
-        private static MediaElement videoObject;
-        // 天気取得の関数を呼び出す時間トリガー
-        private DispatcherTimer darksky_request_manage;
-        private static string hh;
+        private static MediaElement videoObject1;
+        private static MediaElement videoObject2;
+        //天気情報を取得 && 画像透過の関数を呼び出す時間トリガー
+        private DispatcherTimer transmission_image;
+        //透過speedの調整用
+        private int transmission_speed;
 
         public PseudoLivePage()
         {
             this.InitializeComponent();
             // set MediaElement into static variable
-            videoObject = PseudoLivePlayer;
-
+            videoObject1 = PseudoLivePlayer1;
+            videoObject2 = PseudoLivePlayer2;
             this.Loaded += UpdateVideo;
-
-            // 天気取得関数の時限起動設定
-            this.darksky_request_manage = new DispatcherTimer();
-            //this.darksky_request_manage.Interval = TimeSpan.FromSeconds(30);
-            this.darksky_request_manage.Interval = TimeSpan.FromMinutes(5);
-            this.darksky_request_manage.Tick += UpdateVideo;
-            this.darksky_request_manage.Start();
+    
+            // 画面の透過率を変える設定
+            this.transmission_image = new DispatcherTimer();
+            this.transmission_image.Interval = TimeSpan.FromSeconds(1);
+            this.transmission_image.Tick += TransmissionImage;
+            this.transmission_image.Start();
         }
 
-        private static async void ReadVideo2(string weather)
+        private static async void ReadVideo2(string weather,int key)
         {
             int hh = 0;
             int mm = 0;
-            
+            string fileName = "";
+
             DateTime now = DateTime.Now;
 
             //例)8,9,10,11,12→10 | 13.14,15,16,17→15 | 18,19,20,21,22→20
             int real_minute = int.Parse(now.ToString("mm"));
-            int judge = real_minute % 10;
+            /*int judge = real_minute % 10;
             if (judge == 1) { mm = real_minute - 1; }
             else if (judge == 2) { mm = real_minute - 2; }
             else if (judge == 3) { mm = real_minute + 2; }
@@ -69,22 +71,67 @@ namespace VirtualWindowUWP
             else if (judge == 9) { mm = real_minute + 1; }
             else { mm = real_minute;  }
 
-            if (mm == 60) { hh = 1 + int.Parse(now.ToString("HH")); }
-            else { hh = int.Parse(now.ToString("HH")); }
-            string fileName = now.ToString("MM") + "_" + weather + "_" + hh + "_" + mm + ".mp4";
+            if ( mm <= 2 || 58 <= mm ) //00分のときの処理
+            {
+                hh = 1 + int.Parse(now.ToString("HH"));
+                Debug.WriteLine(mm);
+                mm = 0;
+                fileName = now.ToString("MM") + "_" + hh + "0" + mm + "_" + weather + ".mp4";
+            }
+            else if (mm == 5) {
+                hh = int.Parse(now.ToString("HH"));
+                fileName = now.ToString("MM") + "_" + hh + "0" + mm + "_" + weather + ".mp4";
+            }
+            else
+            {
+                hh = int.Parse(now.ToString("HH"));
+                fileName = now.ToString("MM") + "_" + hh + "" + mm + "_" + weather + ".mp4";
+            }*/
+            if (real_minute % 2 == 1) { mm = real_minute + 1; }
+            else { mm = real_minute; }
+
+            if (mm == 59) //00分のときの処理
+            {
+                hh = 1 + int.Parse(now.ToString("HH"));
+                Debug.WriteLine(mm);
+                mm = 0;
+                fileName = now.ToString("MM") + "_" + hh + "0" + mm + "_" + weather + ".mp4";
+            }
+            else if (mm == 2 || mm == 4 || mm == 6 || mm == 8)
+            {
+                hh = int.Parse(now.ToString("HH"));
+                fileName = now.ToString("MM") + "_" + hh + "0" + mm + "_" + weather + ".mp4";
+            }
+            else
+            {
+                hh = int.Parse(now.ToString("HH"));
+                fileName = now.ToString("MM") + "_" + hh + "" + mm + "_" + weather + ".mp4";
+            }
+
             Debug.WriteLine(fileName);
-            StorageFolder targetDir = await StorageFolder.GetFolderFromPathAsync("C:\\Users\\smura\\Videos\\" + now.ToString("MM") + "\\" + weather);
+            StorageFolder targetDir = await StorageFolder.GetFolderFromPathAsync("C:\\Users\\smura\\Videos\\stockvideo\\" + now.ToString("MM") + "\\" + weather);
             StorageFile targetVideo = await targetDir.GetFileAsync(fileName);
 
             var stream = await targetVideo.OpenAsync(Windows.Storage.FileAccessMode.Read);
 
-            videoObject.SetSource(stream, targetVideo.ContentType);
+            if (key == 0)
+            {
+                videoObject1.SetSource(stream, targetVideo.ContentType);
+                videoObject2.SetSource(stream, targetVideo.ContentType);
+            }
+            else if (key == 1)
+            {
+                videoObject1.SetSource(stream, targetVideo.ContentType);
+            }
+            else if (key == 2){
+                videoObject2.SetSource(stream, targetVideo.ContentType);
+            }
         }
 
         private async void UpdateVideo(object sender, object e)
         {
-            string currentWeather = await TestSync();
-            ReadVideo2(currentWeather);
+            string get_icon = await TestSync();
+            ReadVideo2(get_icon, 0);
         }
 
         private static async Task<string> TestSync()
@@ -120,16 +167,50 @@ namespace VirtualWindowUWP
             //snow
             else if (weather == "snow" || weather == "sleet")
             {
-                icon = "sonwly";
+                icon = "snowy";
             }
             //foggy
             else if (weather == "fog")
             {
                 icon = "foggy";
             }
-
             return icon;
         }
 
+        private async void TransmissionImage(object sender, object e)
+        {
+            transmission_speed += 1;
+            Debug.WriteLine(transmission_speed);
+
+            //透過を率を変更する
+            //（詳細）開始10秒で透過0%，4分50秒から透過開始
+            if (transmission_speed <= 2)
+            {
+                videoObject2.Opacity = 0.1 * transmission_speed * 5;
+            }
+            else if (118 <= transmission_speed && transmission_speed <= 120) {
+                videoObject2.Opacity = 0.1 * (120 - transmission_speed) * 5;
+            }
+
+            /*if (transmission_speed <= 10)
+            {
+                videoObject2.Opacity = 0.1 * transmission_speed;
+            }
+            else if (290 <= transmission_speed && transmission_speed <= 300)
+            {
+                videoObject2.Opacity = 0.1 * (300 - transmission_speed);
+            }*/
+
+            //動画更新
+            if (transmission_speed == 2) {
+                string get_icon = await TestSync();
+                ReadVideo2(get_icon,1);
+            }
+            else if (transmission_speed == 120) {
+                string get_icon = await TestSync();
+                ReadVideo2(get_icon,2);
+                transmission_speed = 0;
+            }
+        }
     }
 }
